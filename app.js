f2aabfba1f07a12e8015a21a07ef26044a3c30d9
subhi1608 <sarveshb.bhatt@gmail.com>
2024-04-22 11:54:42 +0530

    removed unused code

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
@@ -9,32 +7,18 @@ const api = require("./api/index");
 const app = express();
 const PORT = process.env.PORT || 8000;
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", api);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	next(createError(404));
-});
 
-// error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render("error");
-});
+app.get("/",(req,res)=>{
+	res.send({status:200,message:"running"})
+})
 
 app.listen(PORT, () => {
 	console.log(`Server running at PORT ${PORT}`);
