29d15c50dbb15a39400d340fdf4b3bb282c16ac0
subhi1608 <sarveshb.bhatt@gmail.com>
2026-05-24 11:49:47 +0530

    chore: install bullmq + ioredis, wire REDIS_URL in test env

--- server/__tests__/setup.ts
+++ server/__tests__/setup.ts
@@ -1,3 +1,4 @@
 process.env.TOKEN = 'test-jwt-access-secret-that-is-long-enough-for-256bit';
 process.env.REFRESH_TOKEN_SECRET = 'test-jwt-refresh-secret-that-is-long-enough-for-256bit';
 process.env.NODE_ENV = 'test';
+process.env.REDIS_URL = process.env.REDIS_URL ?? 'redis://localhost:6379';
